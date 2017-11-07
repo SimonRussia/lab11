e88c014378404a12153945ee02f8c0934bcedf6c
SimonRussia <git.streisand@gmail.com>
2017-11-07 14:28:19 +0300

    Update demo.cpp

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -1,4 +1,6 @@
 #include <print.hpp>
+#include <fstream>
+#include <cstring>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
@@ -14,54 +16,57 @@ try {
       ("help,h", "Help screen")
       ("output", value<std::string>()/*->notifier(out_func)*/, "Output");
 
-    variables_map vm;
-    store(parse_command_line(argc, argv, desc), vm);
+    variables_map vm_console;
+    variables_map vm_file;
+    store(parse_command_line(argc, argv, desc), vm_console);
+    notify(vm_console);
 
     std::string gAddress = getenv("HOME");
     gAddress += "/.config/demo.cfg";
     const char* _gAddress = gAddress.c_str();
 
-
     if(exists(gAddress))
-    	store(parse_config_file<char>( _gAddress, desc ), vm);
+    	store(parse_config_file<char>( _gAddress, desc ), vm_file);
 
-    notify(vm);
+    notify(vm_file);
 
-    if (vm.count("help")) {
+    if (vm_console.count("help") || vm_file.count("help")) {
       std::cout << desc << '\n';
-    }
+    } else if(vm_console.count("output")) {
 
-    else if (getenv("DEMO_OUTPUT") != nullptr) {
-  		std::cout << "ENV" << std::endl;
+      std::cout << "Output in >> " << vm_console["output"].as<std::string>() << '\n';
+      std::ofstream out(vm_console["output"].as<std::string>(), std::ios_base::app);
+      while(std::cin >> text) {
+        print(text, out);
+        out << std::endl;
+      }
+      out.close();
 
+    } else if (getenv("DEMO_OUTPUT") != nullptr) {
+        std::cout << "ENV" << std::endl;
         std::string _trAddress = getenv("DEMO_OUTPUT");
         std::ofstream out(_trAddress, std::ios_base::app);
         while(std::cin >> text) {
-			print(text, out);
-			out << std::endl;
-		}
+          print(text, out);
+          out << std::endl;
+        }
         out.close();
-  	}
-
-  	else if (vm.count("output")) {
-  		std::cout << "Output in >> " << vm["output"].as<std::string>() << '\n';
-  		std::ofstream out(vm["output"].as<std::string>(), std::ios_base::app);
-		while(std::cin >> text) {
-			print(text, out);
-			out << std::endl;
-		}
-		out.close();
-  	}
-
-  	else {
+    } else if (vm_file.count("output")) {
+  		std::cout << "Output in >> " << vm_file["output"].as<std::string>() << '\n';
+  		std::ofstream out(vm_file["output"].as<std::string>(), std::ios_base::app);
+  		while(std::cin >> text) {
+  			print(text, out);
+  			out << std::endl;
+  		}
+  		out.close();
+    } else {
   		std::cout << "DEFAULT" << std::endl;
-
-		std::ofstream out("default.log", std::ios_base::app);
-		while(std::cin >> text) {
-			print(text, out);
-			out << std::endl;
-		}
-		out.close();
+  		std::ofstream out("default.log", std::ios_base::app);
+  		while(std::cin >> text) {
+  			print(text, out);
+  			out << std::endl;
+  		}
+  		out.close();
   	}
 
   } catch (const error &ex) {
